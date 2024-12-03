bbc2998becec5ae07f082ed4f7af362a36aad00a
RusithTharindu <rusiththarindu2@gmail.com>
2024-12-03 16:16:59 +0530

    fix: Profile interface fixes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -31,25 +31,20 @@ const ProfilePage = () => {
             <CircleUser size={100} className="my-5 opacity-70"/>
             <div className="text-center">
                 <div className="flex flex-row gap-2">
-                    <p className="text-slate-500/80">Username:</p>
+                    <p className="text-slate-500/80">Username :</p>
                     <p className="">{userData.username}</p>
                 </div>
                 <div className="flex flex-row gap-2">
-                    <p className="text-slate-500/80">Name:</p>
+                    <p className="text-slate-500/80">Name :</p>
                     <p className="">{userData.name}</p>
                 </div>
                 <div className="flex flex-row gap-2">
-                    <p className="text-slate-500/80">Email:</p>
+                    <p className="text-slate-500/80">Email :</p>
                     <p className="">{userData.email}</p>
                 </div>
                 
             </div>
         </div>
-        {/* <h1 className="text-3xl font-bold mb-6">{userData.name}</h1>
-      <h1 className="text-3xl font-bold mb-6">{userData.email}</h1> 
-      <h1 className="text-3xl font-bold mb-6">{userData.username}</h1>  */}
-
-
     </div>
   );
 };
