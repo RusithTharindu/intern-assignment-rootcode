23f933b32ccd4221f7b24ed35602ad484317eea1
RusithTharindu <rusiththarindu2@gmail.com>
2024-12-03 15:54:16 +0530

    refactor: BUtton classname prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 interface ButtonProps {
-    children: React.ReactNode;
-    onClickFunc: () => void;
-    classes?: string;
+  children: React.ReactNode;
+  onClickFunc: () => void;
+  classes?: string[];
 }
 
-const Button: React.FC<ButtonProps> = ({children, onClickFunc, classes}) => {
+const Button: React.FC<ButtonProps> = ({ children, onClickFunc, classes = [] }) => {
+  const classNames = classes.join(' ');
+
   return (
     <button
-        className={`border border-black hover:border-black/50 bg-yellow-300 text-black hover:text-black/50 font-bold p-2 rounded-xl ${classes}`}
-        onClick={onClickFunc}
-    >{children}</button>
-  )
-}
+      className={`border border-black hover:border-black/50 bg-yellow-300 text-black hover:text-black/50 font-bold p-2 rounded-xl ${classNames}`}
+      onClick={onClickFunc}
+    >
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
